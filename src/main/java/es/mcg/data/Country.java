5e8d99c8f808709384ddb18fa31e8a4ee9bc85df
Manuel Cano Gil <mcangil786@g.educaand.es>
2022-11-24 08:59:04 +0100

    Country

--- src/main/java/es/mcg/data/Country.java
+++ src/main/java/es/mcg/data/Country.java
@@ -3,4 +3,32 @@ package es.mcg.data;
 public class Country {
     private Integer id;
     private String name;
+
+    public Country()
+    {
+        this.id = 0;
+        this.name = "";
+    }
+
+    public Integer getId() 
+    {
+        return id;
+    }
+
+    public String getName() 
+    {
+        return name;
+    }
+
+    public void setId(Integer id) 
+    {
+        this.id = id;
+    }
+
+    public void setName(String name) 
+    {
+        this.name = name;
+    }
+
+    
 }
