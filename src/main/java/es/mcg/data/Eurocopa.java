5d6279bcee3ccd26308e42c836b226af133a767f
Manuel Cano Gil <mcangil786@g.educaand.es>
2022-11-24 09:00:25 +0100

    Eurocopa

--- src/main/java/es/mcg/data/Eurocopa.java
+++ src/main/java/es/mcg/data/Eurocopa.java
@@ -10,9 +10,15 @@ public class Eurocopa {
     private LocalTime kick_off;
     private Competition competition;
     private Season season;
+    private HomeTeam home_team;
+    private AwayTeam away_team;
     private Integer home_score;
     private Integer away_score;
     private String match_status, match_status_360;
     private LocalDateTime last_updated, last_updated360;
-
+    private MetaData metadata;
+    private Integer match_week;
+    private CompetitionStage competition_stage;
+    private Stadium stadium;
+    private Referee referee;
 }
