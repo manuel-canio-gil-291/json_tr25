0f8daf5605f83d9d224aea91a25d0a9d19f8532d
Manuel Cano Gil <mcangil786@g.educaand.es>
2022-11-24 08:58:47 +0100

    Competition

--- src/main/java/es/mcg/data/Competition.java
+++ src/main/java/es/mcg/data/Competition.java
@@ -3,4 +3,42 @@ package es.mcg.data;
 public class Competition {
     private Integer competition_id;
     private String country_name, competition_name;
+
+    public Competition()
+    {
+        this.competition_id = 0;
+        this.country_name = "";
+        this.competition_name = "";
+    }
+
+    public Integer getCompetition_id() 
+    {
+        return competition_id;
+    }
+
+    public String getCountry_name() 
+    {
+        return country_name;
+    }
+
+    public String getCompetition_name() 
+    {
+        return competition_name;
+    }
+
+    public void setCompetition_id(Integer competition_id) 
+    {
+        this.competition_id = competition_id;
+    }
+
+    public void setCountry_name(String country_name) 
+    {
+        this.country_name = country_name;
+    }
+
+    public void setCompetition_name(String competition_name) 
+    {
+        this.competition_name = competition_name;
+    }
+    
 }
