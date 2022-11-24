8c021b7e736d86b1c280646a0a3a4398ba8ceb56
Manuel Cano Gil <mcangil786@g.educaand.es>
2022-11-24 09:00:40 +0100

    Season

--- src/main/java/es/mcg/data/Season.java
+++ src/main/java/es/mcg/data/Season.java
@@ -3,4 +3,30 @@ package es.mcg.data;
 public class Season {
     private Integer season_id;
     private String season_name;
+
+    public Season()
+    {
+        this.season_id = 0;
+        this.season_name = "";
+    }
+
+    public Integer getSeason_id() 
+    {
+        return season_id;
+    }
+
+    public String getSeason_name() 
+    {
+        return season_name;
+    }
+
+    public void setSeason_id(Integer season_id) 
+    {
+        this.season_id = season_id;
+    }
+
+    public void setSeason_name(String season_name) 
+    {
+        this.season_name = season_name;
+    }
 }
